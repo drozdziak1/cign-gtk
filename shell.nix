0f4586d406847d8c3fe33eb980648617470b26e1
Stan Drozd <drozdziak1@gmail.com>
2021-02-25 01:07:32 +0100

    nix: haskell.nix setup

--- shell.nix
+++ shell.nix
@@ -1,17 +1,10 @@
 let
   sources = import nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  common-deps = pkgs.callPackage (import ./common-deps.nix) {};
 in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    gobject-introspection
-    gtk3
-    harfbuzz
-    pango
-    pkg-config
-    stack
-    zlib
-  ] ++ (
+  nativeBuildInputs = with pkgs; [ stack stack2nix ] ++ common-deps ++ (
     with pkgs.haskellPackages;
     [ haskell-language-server cabal-install ]
   );
