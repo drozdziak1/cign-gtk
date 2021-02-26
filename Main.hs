cff993499ac3fd53458fbd5d5923c247c0f6885b
Stan Drozd <drozdziak1@gmail.com>
2021-02-26 02:24:58 +0100

    Add Ignoring and Refreshing

--- Main.hs
+++ Main.hs
@@ -6,7 +6,8 @@ module Main where
 
 import qualified Cign as Cign
 import qualified Cli as Cli
-import Data.Text.Lazy
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Text.Lazy as TXT
 import Data.Text.Lazy.Encoding
 import GI.Gdk.Enums
 import qualified GI.Gtk as Gtk
@@ -16,9 +17,9 @@ import Options.Applicative
 import System.Exit
 import System.Process.Typed
 
-data State = State {stateBuffer :: Gtk.TextBuffer}
+data State = State {stateBuffer :: Gtk.TextBuffer, exitCode :: ExitCode, stateExtraArgs :: String}
 
-data Event = Closed | Refresh [Char]
+data Event = Closed | Check | Ignore
 
 main :: IO ()
 main = do
@@ -28,19 +29,16 @@ main = do
   case code of
     ExitFailure _ -> do
       buf <- Gtk.new Gtk.TextBuffer []
-      Gtk.setTextBufferText buf $
-        toStrict $
-          decodeUtf8 $
-            "Standard out:\n" <> out <> "\nStandard err:\n" <> err
-      _ <-
+      Gtk.setTextBufferText buf $ TXT.toStrict $ mkBufferText out err
+      finalState <-
         run
           App
             { view = view',
               update = update',
               inputs = [],
-              initialState = State {stateBuffer = buf}
+              initialState = State {stateBuffer = buf, exitCode = code, stateExtraArgs = extraArgs}
             }
-      exitWith code
+      exitWith $ exitCode finalState
     _ -> pure ()
 
 view' :: State -> AppView Gtk.Window Event
@@ -53,11 +51,45 @@ view' state =
         ( const
             (True, Closed)
         ),
-      #typeHint := WindowTypeHintDialog
+      #typeHint := WindowTypeHintDialog -- Make sure this floats on tiling WMs
     ]
-    $ widget Gtk.TextView [#editable := False, #buffer := stateBuffer state]
+    $ container Gtk.Box [#spacing := 10] $
+      [ widget
+          Gtk.TextView
+          [ #editable := False,
+            #buffer := stateBuffer state
+          ],
+        container Gtk.Box [#orientation := Gtk.OrientationVertical] $
+          [ widget
+              Gtk.Button
+              [ #label := (TXT.toStrict $ TXT.pack "Ignore"),
+                on
+                  #clicked
+                  Ignore
+              ],
+            widget
+              Gtk.Button
+              [ #label := (TXT.toStrict $ TXT.pack "Check"),
+                on
+                  #clicked
+                  Check
+              ]
+          ]
+      ]
 
 update' :: State -> Event -> Transition State Event
 update' state e = case e of
   Closed -> Exit
-  Refresh _b -> Transition state $ (return Nothing)
+  Check -> Transition state $ newEvent
+    where
+      newEvent = do
+        (_code, out, err) <- readProcess $ Cign.runCign $ stateExtraArgs state
+
+        let txt = mkBufferText out err
+        Gtk.setTextBufferText (stateBuffer state) $ TXT.toStrict txt
+        return Nothing
+  Ignore -> Transition state {exitCode = ExitSuccess} $ pure $ Just Closed
+
+mkBufferText :: BS.ByteString -> BS.ByteString -> TXT.Text
+mkBufferText out err =
+  "Standard out:\n" <> decodeUtf8 out <> "\nStandard err:\n" <> decodeUtf8 err
