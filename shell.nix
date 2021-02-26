d8e3a441b5b930d6d075d1393eb67a1f16280275
Stan Drozd <drozdziak1@gmail.com>
2021-02-26 02:24:16 +0100

    Jerry-rig ghc8104 access for stack with unstable nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,11 +1,20 @@
 let
   sources = import nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs-unstable = import sources.nixpkgs-unstable {};
+  ghc-olay = self: super: {
+    haskell =  {
+      compiler.ghc8104 = pkgs-unstable.haskell.compiler.ghc8104;
+    } // super.haskell;
+  };
+  vanilla = import sources.nixpkgs {};
+  pkgs = import sources.nixpkgs { overlays = [ ghc-olay ]; };
   common-deps = pkgs.callPackage (import ./common-deps.nix) {};
 in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ stack stack2nix ] ++ common-deps ++ (
+  nativeBuildInputs = with pkgs; [ stack nix ] ++ common-deps ++ (
     with pkgs.haskellPackages;
     [ haskell-language-server cabal-install ]
   );
+  NIX_CFLAGS_COMPILE="";
+  NIX_PATH = "nixpkgs=${pkgs-unstable.path}";
 }
