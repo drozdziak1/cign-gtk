b29be2b17952586afc597f96805bf324044dc7f6
Stan Drozd <drozdziak1@gmail.com>
2021-03-05 02:11:56 +0100

    Add gitignore.nix, bump niv deps

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@
 , haskellNix ? import sources.haskell-nix {}
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
-
   # import nixpkgs with overlays
 , pkgs ? import nixpkgsSrc nixpkgsArgs
+, gitignoreSource ? (import sources.gitignore-nix {}).gitignoreSource
 }: pkgs.haskell-nix.project {
   # 'cleanGit' cleans a source directory based on the files known by git
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "cign-gtk";
-    src = ./.;
+    src = gitignoreSource ./.;
   };
 }
