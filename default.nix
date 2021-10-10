3641490b0371f40924150c91a166d2fd115cffb8
Stan Drozd <drozdziak1@gmail.com>
2021-10-10 15:34:39 +0200

    default.nix: pkgs hack

--- default.nix
+++ default.nix
@@ -1,13 +1,14 @@
 { sources ? import ./nix/sources.nix
+, system ? builtins.currentSystem
 , haskellNix ? import sources.haskell-nix {}
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
   # import nixpkgs with overlays
-, pkgs ? import nixpkgsSrc nixpkgsArgs
+, pkgs-overlaid ? import nixpkgsSrc (nixpkgsArgs // { inherit system; })
 , gitignoreSource ? (import sources.gitignore-nix {}).gitignoreSource
-}: (pkgs.haskell-nix.project {
+}: (pkgs-overlaid.haskell-nix.project {
   # 'cleanGit' cleans a source directory based on the files known by git
-  src = pkgs.haskell-nix.haskellLib.cleanGit {
+  src = pkgs-overlaid.haskell-nix.haskellLib.cleanGit {
     name = "cign-gtk";
     src = gitignoreSource ./.;
   };
